346dae5c02ee3b73e83661ad504b3f0d1793f66c
Jordan Hunt <jordan.hunt@g.austincc.edu>
2026-08-23 07:43:25 -0500

    Trimmed level.h

--- level.h
+++ level.h
@@ -1,14 +1,23 @@
 #ifndef LEVEL_H
 #define LEVEL_H
-#include "game.h"
-#include <SDL3/SDL_rect.h>
-#include <math.h>
+#include "config.h"
+#include <SDL3/SDL.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+typedef struct Level {
+  int width;
+  int height;
+  unsigned char *tiles;
+  float playerSpawnX;
+  float playerSpawnY;
+  bool hasPlayerSpawn;
+} Level;
+/**
+ * Loads a level from a text file into `level`. On failure, `level` is left
+ * untouched (any tiles it already owned are freed first, but no partial data is
+ * committed).
+ * */
+bool LevelLoadFromFile(Level *level, const char *filePath);
 void LevelFree(Level *level);
-void LevelRender(const Level *level, SDL_Renderer *renderer, float cameraX,
-                 float cameraY);
-bool LevelLoadFromFile(Level *level, const char *filepath);
 #endif
