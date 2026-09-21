260b730bb65d4423e9663a082b97b53a4ea3f4c3
Jordan "Heroic" Hunt <jordan.hunt@g.austincc.edu>
2026-09-21 15:19:37 -0500

    More merge fixes

--- include/gameworld.h
+++ include/gameworld.h
@@ -16,13 +16,9 @@ typedef struct GameWorld {
   Projectile projectiles[MAX_PROJECTILES];
   char currentLevelPath[MAX_LEVEL_PATH_LENGTH];
 } GameWorld;
-<<<<<<< HEAD
-typedef enum Direction { LEFT, RIGHT, UP, DOWN } Direction;
-=======
 
 typedef enum Direction { LEFT, RIGHT, UP, DOWN } Direction;
 
->>>>>>> a1abf27e7b3eddc7c7216ebca890fd6222e36cfc
 /**
  * Loads 'levelPath' into the world, resets the camera, places the player at the
  * level's spawn point, and clears any existing projectiles
@@ -40,18 +36,10 @@ void GameWorldUpdate(GameWorld *gameWorld, const Input *input, float deltaTime);
 void GameWorldRender(const GameWorld *gameWorld, SDL_Renderer *renderer,
                      SDL_Texture *projectileTexture,
                      SDL_Texture *levelEnemyTexture, SDL_Texture *doorTexture);
-<<<<<<< HEAD
-=======
-
->>>>>>> a1abf27e7b3eddc7c7216ebca890fd6222e36cfc
 bool GameWorldChangeLevelAtDoor(GameWorld *gameWorld,
                                 const char *targetLevelPath, int targetDoorX,
                                 int targetDoorY, int spawnOffsetX,
                                 int spawnOffSetY);
-<<<<<<< HEAD
-=======
-
->>>>>>> a1abf27e7b3eddc7c7216ebca890fd6222e36cfc
 /**
  * If "use" was just pressed and the player is standing in a door, transitions
  * the world to that door's target room. Returns true if a transition
@@ -59,17 +47,9 @@ bool GameWorldChangeLevelAtDoor(GameWorld *gameWorld,
  * the world just changed underneath it.
  */
 bool GameWorldHandleDoorUse(GameWorld *gameWorld, const Input *input);
-<<<<<<< HEAD
 /**
  * If the player walks of the edge of a map
  */
 bool GameWorldChangeLevelAtEdge(GameWorld *gameWorld, const char *path,
                                 Direction exitDirection);
-=======
-
-/**
- * If the player walks of the edge of a map 
- */
-bool GameWorldChangeLevelAtEdge(GameWorld *gameWorld, const char *path, Direction exitDirection)
->>>>>>> a1abf27e7b3eddc7c7216ebca890fd6222e36cfc
 #endif
